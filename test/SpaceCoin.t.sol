bb54dd5019ef5b57f7dd1895c5af3e2d4df756dd
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-01-02 16:13:01 -0300

    Added new unit test files

--- test/SpaceCoin.t.sol
+++ test/SpaceCoin.t.sol
@@ -22,4 +22,29 @@ contract SpaceCoinTest is Test {
         coin = new SpaceCoin(treasury, coinCreator);
         vm.stopPrank();
     }
+
+    function test_SpaceCoin_transfer_TaxEnabledAndAmountLessThan100() public {
+        vm.startPrank(coinCreator);
+        coin.transfer(bob, 50);
+        vm.stopPrank();
+        assertTrue(coin.balanceOf(bob) == 49, "bob should have 49 coins");
+        assertTrue(coin.balanceOf(treasury) == 350001, "treasury should have 350001 coins");
+    }
+
+    function test_SpaceCoin_transfer_TaxEnabledAndAmountGreaterThan100() public {
+        vm.startPrank(coinCreator);
+        coin.transfer(bob, 150);
+        vm.stopPrank();
+        assertTrue(coin.balanceOf(bob) == 148, "bob should have 148 coins");
+        assertTrue(coin.balanceOf(treasury) == 350002, "treasury should have 350002 coins");
+    }
+
+    function test_SpaceCoin_transfer_TaxDisabled() public {
+        vm.startPrank(coinCreator);
+        coin.toggleTax();
+        coin.transfer(bob, 10000);
+        vm.stopPrank();
+        assertTrue(coin.balanceOf(bob) == 10000, "bob should have 10000 coins");
+        assertTrue(coin.balanceOf(treasury) == 350000, "treasury should have 350000 coins");
+    }
 }
