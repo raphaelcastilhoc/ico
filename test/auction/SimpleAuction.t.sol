50331df40910bffbd23e90e3c6b5535cb0c239d5
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-01-10 18:50:12 -0300

    Added new unit test files

---
+++ test/auction/SimpleAuction.t.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/auction/SimpleAuction.sol";
+
+contract SimpleAuctionTest is Test {
+
+    address payable creator = payable(address(0x123));
+    address jill = address(0x456);
+    address chris = address(0x789);
+
+    SimpleAuction simpleAuction;
+
+    function setUp() public {
+
+        vm.deal(creator, 10 ether);
+        vm.deal(jill, 10 ether);
+        vm.deal(chris, 10 ether);
+
+        vm.startPrank(creator);
+        simpleAuction = new SimpleAuction(7 days, creator);
+        vm.stopPrank();
+    }
+
+    function test_bid_FailWhenAuctionAlreadyEnded() public {
+    vm.startPrank(jill);
+
+    uint256 futureTimestamp = uint48(block.timestamp) + 8 days;
+    vm.warp(futureTimestamp);
+
+    vm.expectRevert(abi.encodeWithSelector(SimpleAuction.AuctionAlreadyEnded.selector));
+    simpleAuction.bid{value: 1 ether}();
+    
+    vm.stopPrank();
+}
+
+    function test_bid_FailWhenBidIsNotHighEnough() public {
+    vm.startPrank(jill);
+
+    simpleAuction.bid{value: 2 ether}();
+
+    vm.expectRevert(abi.encodeWithSelector(SimpleAuction.BidNotHighEnough.selector, 2 ether));
+    simpleAuction.bid{value: 1 ether}();
+    
+    vm.stopPrank();
+}
+
+    function test_withdraw_FailWhenSenderHasPendingReturns() public {
+    vm.startPrank(jill);
+
+    simpleAuction.bid{value: 2 ether}();
+
+    vm.startPrank(chris);
+
+    simpleAuction.bid{value: 3 ether}();
+
+    vm.startPrank(jill);
+
+    bool result = simpleAuction.withdraw();
+    
+    vm.stopPrank();
+
+    assertTrue(result);
+    assertEq(jill.balance, 10 ether);
+}
+
+    function test_withdraw_SuccessfulWithdraw() public {
+    vm.startPrank(jill);
+
+    simpleAuction.bid{value: 2 ether}();
+
+    vm.startPrank(chris);
+
+    simpleAuction.bid{value: 3 ether}();
+
+    vm.startPrank(jill);
+
+    bool result = simpleAuction.withdraw();
+    
+    vm.stopPrank();
+
+    assertTrue(result);
+    assertEq(jill.balance, 10 ether);
+}
+
+    function test_withdraw_FailWhenAmountIsZero() public {
+    vm.startPrank(jill);
+
+    bool result = simpleAuction.withdraw();
+    
+    vm.stopPrank();
+
+    assertTrue(result);
+}
+
+    function test_auctionEnd_FailWhenAuctionNotYetEnded() public {
+    vm.expectRevert(SimpleAuction.AuctionNotYetEnded.selector);
+    simpleAuction.auctionEnd();
+}
+}
