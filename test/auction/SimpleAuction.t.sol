24d6bd9f7871d202a3402b2272f997e5832c9358
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-01-24 13:37:03 -0300

    Added new unit test files

--- test/auction/SimpleAuction.t.sol
+++ test/auction/SimpleAuction.t.sol
@@ -21,46 +21,6 @@ contract SimpleAuctionTest is Test {
         vm.stopPrank();
     }
 
-    function test_bid_FailWhenBidIsNotHighEnough() public {
-    vm.startPrank(jill);
-
-    simpleAuction.bid{value: 1 ether}();
-
-    vm.expectRevert(abi.encodeWithSelector(SimpleAuction.BidNotHighEnough.selector, 1 ether));
-    simpleAuction.bid{value: 1 ether}();
-    
-    vm.stopPrank();
-}
-
-    function test_withdraw_SuccessfulWithdraw() public {
-    vm.startPrank(jill);
-
-    simpleAuction.bid{value: 2 ether}();
-
-    vm.startPrank(chris);
-
-    simpleAuction.bid{value: 3 ether}();
-
-    vm.startPrank(jill);
-
-    bool result = simpleAuction.withdraw();
-    
-    vm.stopPrank();
-
-    assertTrue(result);
-    assertEq(jill.balance, 10 ether);
-}
-
-    function test_withdraw_FailWhenAmountIsZero() public {
-    vm.startPrank(jill);
-
-    bool result = simpleAuction.withdraw();
-    
-    vm.stopPrank();
-
-    assertTrue(result);
-}
-
     function test_auctionEnd_FailWhenAuctionNotYetEnded() public {
     vm.expectRevert(SimpleAuction.AuctionNotYetEnded.selector);
     simpleAuction.auctionEnd();
