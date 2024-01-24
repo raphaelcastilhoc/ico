4719c73eebfa2080be26ddcc53ad167be97f8379
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-01-24 18:56:41 -0300

    Added new unit test files

--- test/auction/SimpleAuction.t.sol
+++ test/auction/SimpleAuction.t.sol
@@ -44,13 +44,33 @@ contract SimpleAuctionTest is Test {
     vm.stopPrank();
 }
 
+    function test_withdraw_SuccessfulWithdraw() public {
+    vm.startPrank(jill);
+
+    simpleAuction.bid{value: 2 ether}();
+
+    vm.startPrank(chris);
+
+    simpleAuction.bid{value: 3 ether}();
+
+    vm.startPrank(jill);
+
+    bool result = simpleAuction.withdraw();
+    
+    vm.stopPrank();
+
+    assertTrue(result);
+    assertEq(jill.balance, 10 ether);
+}
+
     function test_withdraw_FailWhenAmountIsZero() public {
     vm.startPrank(jill);
 
     bool result = simpleAuction.withdraw();
-    assertTrue(result);
-
+    
     vm.stopPrank();
+
+    assertTrue(result);
 }
 
     function test_auctionEnd_FailWhenAuctionNotYetEnded() public {
