4fe1bb2ec8f4556a59b52c807db3a11c808a114c
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-01-24 18:18:52 -0300

    Added new unit test files

--- test/auction/SimpleAuction.t.sol
+++ test/auction/SimpleAuction.t.sol
@@ -21,41 +21,40 @@ contract SimpleAuctionTest is Test {
         vm.stopPrank();
     }
 
-    function test_auctionEnd_FailWhenAuctionNotYetEnded() public {
-        vm.expectRevert(SimpleAuction.AuctionNotYetEnded.selector);
-        simpleAuction.auctionEnd();
-    }
-
     function test_bid_FailWhenAuctionAlreadyEnded() public {
-        vm.startPrank(jill);
+    vm.startPrank(jill);
+
+    uint256 futureTimestamp = uint48(block.timestamp) + 8 days;
+    vm.warp(futureTimestamp);
+
+    vm.expectRevert(abi.encodeWithSelector(SimpleAuction.AuctionAlreadyEnded.selector));
+    simpleAuction.bid{value: 1 ether}();
     
-        uint256 futureTimestamp = uint48(block.timestamp) + 8 days;
-        vm.warp(futureTimestamp);
-    
-        vm.expectRevert(abi.encodeWithSelector(SimpleAuction.AuctionAlreadyEnded.selector));
-        simpleAuction.bid{value: 1 ether}();
-        
-        vm.stopPrank();
-    }
+    vm.stopPrank();
+}
 
     function test_bid_FailWhenBidIsNotHighEnough() public {
-        vm.startPrank(jill);
+    vm.startPrank(jill);
+
+    simpleAuction.bid{value: 2 ether}();
+
+    vm.expectRevert(abi.encodeWithSelector(SimpleAuction.BidNotHighEnough.selector, 2 ether));
+    simpleAuction.bid{value: 1 ether}();
     
-        simpleAuction.bid{value: 1 ether}();
-    
-        vm.expectRevert(abi.encodeWithSelector(SimpleAuction.BidNotHighEnough.selector, 1 ether));
-        simpleAuction.bid{value: 1 ether}();
-    
-        vm.stopPrank();
-    }
+    vm.stopPrank();
+}
 
     function test_withdraw_FailWhenAmountIsZero() public {
-        vm.startPrank(jill);
-    
-        bool result = simpleAuction.withdraw();
-        
-        vm.stopPrank();
-    
-        assertTrue(result);
-    }
+    vm.startPrank(jill);
+
+    bool result = simpleAuction.withdraw();
+    assertTrue(result);
+
+    vm.stopPrank();
+}
+
+    function test_auctionEnd_FailWhenAuctionNotYetEnded() public {
+    vm.expectRevert(SimpleAuction.AuctionNotYetEnded.selector);
+    simpleAuction.auctionEnd();
+}
 }
