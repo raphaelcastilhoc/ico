6fa11f1c6dbecb5c9c8362e849dca0d5dd6e2fa0
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-01-25 19:56:45 -0300

    Added new unit test files

--- test/hooks/ProtocolFee.t.sol
+++ test/hooks/ProtocolFee.t.sol
@@ -88,7 +88,9 @@ contract ProtocolFeeTest is Test {
 
     function test_collectProtocolFees_SuccessfulWhenCalledByAnyAddress() public {
     fees.postDispatch{value: FEE}(abi.encodePacked(""), testMessage);
+    assertEq(address(fees).balance, FEE);
     fees.collectProtocolFees();
+    assertEq(address(fees).balance, 0);
     assertEq(bob.balance, FEE);
 }
 
