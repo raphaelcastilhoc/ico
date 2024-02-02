7354d450d2702dd38443e9c74b8ad45a4703aaab
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-02-02 18:33:59 -0300

    Added new unit test files

--- test/SpaceCoin.t.sol
+++ test/SpaceCoin.t.sol
@@ -30,16 +30,10 @@ contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
     assertEq(coin.balanceOf(coinCreator), 149900);
 }
 
-    /**
-* The problem with my previous attempt was that I was trying to transfer 50 tokens from the coinCreator to bob. However, the coinCreator only had 0 tokens, which is less than the amount I was trying to transfer. This resulted in a revert error because the transfer amount exceeded the balance of the coinCreator. To fix this, I need to transfer an amount that is less than or equal to the balance of the coinCreator.
-*/
-function test_transfer_SuccessfulTransferWithoutTax() public {
+    function test_toggleTax_SuccessfulToggleTax() public {
     vm.prank(coinCreator);
     coin.toggleTax();
-    coin.transfer(bob, 0);
 
-    assertEq(coin.balanceOf(bob), 0);
-    assertEq(coin.balanceOf(treasury), 350000);
-    assertEq(coin.balanceOf(coinCreator), 150000);
+    assert(!coin.taxEnabled());
 }
 }
