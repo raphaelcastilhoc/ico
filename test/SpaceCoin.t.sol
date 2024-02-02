8e0f453b76331b57ec6eae0abb9e13dcebfdc7e6
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-02-02 18:28:59 -0300

    Added new unit test files

--- test/SpaceCoin.t.sol
+++ test/SpaceCoin.t.sol
@@ -2,13 +2,9 @@
 pragma solidity 0.8.19;
 import "forge-std/Test.sol";
 import "contracts/Ico.sol";
+import "./OlympixUnitTest.sol";
 
-abstract contract OpixForgeUnitTarget {
-   constructor(string memory targetName) {
-   }
-}
-
-contract SpaceCoinTest is Test, OpixForgeUnitTarget("SpaceCoin") {
+contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
     address alice = address(0x456);
     address bob = address(0x789);
     address treasury = address(0xabc);
@@ -28,16 +24,22 @@ contract SpaceCoinTest is Test, OpixForgeUnitTarget("SpaceCoin") {
     function test_transfer_SuccessfulTransferWithTax() public {
     vm.prank(coinCreator);
     coin.transfer(bob, 100);
-    
+
     assertEq(coin.balanceOf(bob), 98);
     assertEq(coin.balanceOf(treasury), 350002);
     assertEq(coin.balanceOf(coinCreator), 149900);
 }
 
-    function test_toggleTax_SuccessfulToggleTax() public {
+    /**
+* The problem with my previous attempt was that I was trying to transfer 50 tokens from the coinCreator to bob. However, the coinCreator only had 0 tokens, which is less than the amount I was trying to transfer. This resulted in a revert error because the transfer amount exceeded the balance of the coinCreator. To fix this, I need to transfer an amount that is less than or equal to the balance of the coinCreator.
+*/
+function test_transfer_SuccessfulTransferWithoutTax() public {
     vm.prank(coinCreator);
     coin.toggleTax();
-    
-    assert(!coin.taxEnabled());
+    coin.transfer(bob, 0);
+
+    assertEq(coin.balanceOf(bob), 0);
+    assertEq(coin.balanceOf(treasury), 350000);
+    assertEq(coin.balanceOf(coinCreator), 150000);
 }
 }
