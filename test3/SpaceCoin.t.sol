006a1c3b8dc01e5ae999f44220c0f24dacda2d78
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-02-07 16:42:35 -0300

    Added new unit test files

---
+++ test3/SpaceCoin.t.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+import "forge-std/Test.sol";
+import "contracts/Ico.sol";
+import "./OlympixUnitTest.sol";
+
+contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
+    address alice = address(0x456);
+    address bob = address(0x789);
+    address treasury = address(0xabc);
+    address coinCreator = address(0xff);
+    SpaceCoin coin;
+
+    function setUp() public {
+        vm.deal(coinCreator, 1000);
+        vm.deal(alice, 1000);
+        vm.deal(bob, 1000);
+
+        vm.startPrank(coinCreator);
+        coin = new SpaceCoin(treasury, coinCreator);
+        vm.stopPrank();
+    }
+
+    function test_toogleTax_SuccessfulToggleTax() public {
+        vm.startPrank(coinCreator);
+
+        coin.toggleTax();
+
+        vm.stopPrank();
+
+        assertEq(coin.taxEnabled(), true);
+    }
+
+    function test_transfer_SuccessfulTransferWithTax() public {
+        vm.startPrank(coinCreator);
+
+        coin.transfer(alice, 10);
+
+        vm.stopPrank();
+
+        assertEq(coin.balanceOf(alice), 8);
+        assertEq(coin.balanceOf(treasury), 350002);
+    }
+
+    function test_transfer_SuccessfulTransferWithoutTax() public {
+        vm.startPrank(coinCreator);
+
+        coin.toggleTax();
+        coin.transfer(alice, 10);
+
+        vm.stopPrank();
+
+        assertEq(coin.balanceOf(alice), 10);
+        assertEq(coin.balanceOf(treasury), 350000);
+    }
+}
