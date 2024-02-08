fa57ef11c529c2d85460a36a6fb7587309aace4c
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-02-08 18:22:33 -0300

    Added new unit test files

---
+++ test3/BankAccount.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "contracts/BankAccount.sol";
+import "./OlympixUnitTest.sol";
+
+contract BankAccountTest is OlympixUnitTest("BankAccount") {
+    address alice = address(0x3);
+    address bob = address(0x4);
+    address david = address(0x5);
+
+    BankAccount bankAccount;
+
+    function setUp() public {
+        vm.deal(alice, 1000);
+        vm.deal(bob, 1000);
+        vm.deal(david, 1000);
+
+        bankAccount = new BankAccount();
+    }
+
+    /**
+* The problem with my previous attempt was that I didn't call the deposit function with the prank of alice address. So, the balance was being added to the test contract address instead of alice address.
+*/
+function test_withdraw_SuccessfulWithdraw() public {
+    vm.deal(alice, 2000 ether);
+
+    vm.startPrank(alice);
+
+    bankAccount.deposit{value: 10 ether}();
+    bankAccount.withdraw(1 ether);
+
+    vm.stopPrank();
+
+    vm.startPrank(alice);
+
+    assertEq(bankAccount.getBalance(), 9 ether);
+
+    vm.stopPrank();
+
+    assertEq(alice.balance, 1991 ether);
+}
+}
---
+++ test3/CoinTests/SpaceCoin.t.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+import "forge-std/Test.sol";
+import "contracts/Ico.sol";
+import "../OlympixUnitTest.sol";
+
+contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
+    address alice = address(0x456);
+    address bob = address(0x789);
+    address treasury = address(0xabc);
+    address coinCreator = address(0xff);
+    SpaceCoin coin;
+
+    function setUp() public {
+        vm.deal(coinCreator, 1000);
+        vm.deal(alice, 1000);
+        vm.deal(bob, 1000);
+
+        vm.startPrank(coinCreator);
+        coin = new SpaceCoin(treasury, coinCreator);
+        vm.stopPrank();
+    }
+
+    function test_transfer_SuccessfulTransferWithTax() public {
+    vm.prank(coinCreator);
+    coin.transfer(alice, 100);
+
+    assertEq(coin.balanceOf(coinCreator), 149900);
+    assertEq(coin.balanceOf(alice), 98);
+    assertEq(coin.balanceOf(treasury), 350002);
+}
+
+    function test_toggleTax_SuccessfulToggleTax() public {
+    vm.prank(coinCreator);
+    coin.toggleTax();
+
+    assert(!coin.taxEnabled());
+}
+}
