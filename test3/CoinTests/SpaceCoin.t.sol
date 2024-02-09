7c337d3db2aa4496d8258c6165f8b46d72c8257b
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-02-09 18:54:34 -0300

    Added new unit test files

--- test3/CoinTests/SpaceCoin.t.sol
+++ test3/CoinTests/SpaceCoin.t.sol
@@ -24,7 +24,7 @@ contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
     function test_transfer_SuccessfulTransferWithTax() public {
     vm.prank(coinCreator);
     coin.transfer(alice, 100);
-    
+
     assertEq(coin.balanceOf(coinCreator), 149900);
     assertEq(coin.balanceOf(alice), 98);
     assertEq(coin.balanceOf(treasury), 350002);
@@ -33,7 +33,8 @@ contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
     function test_toggleTax_SuccessfulToggleTax() public {
     vm.prank(coinCreator);
     coin.toggleTax();
-    
-    assert(!coin.taxEnabled());
+
+    bool taxStatus = coin.taxEnabled();
+    assert(!taxStatus);
 }
 }
