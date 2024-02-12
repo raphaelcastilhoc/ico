9e6c4a73d718dc877587e719d035db1c6920eee7
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-02-12 12:57:28 -0300

    Added new unit test files

--- test/SpaceCoin.t.sol
+++ test/SpaceCoin.t.sol
@@ -2,10 +2,9 @@
 pragma solidity 0.8.19;
 import "forge-std/Test.sol";
 import "contracts/Ico.sol";
+import "./OlympixUnitTest.sol";
 
-
-contract SpaceCoinTest is Test {
-
+contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
     address alice = address(0x456);
     address bob = address(0x789);
     address treasury = address(0xabc);
@@ -13,7 +12,6 @@ contract SpaceCoinTest is Test {
     SpaceCoin coin;
 
     function setUp() public {
-
         vm.deal(coinCreator, 1000);
         vm.deal(alice, 1000);
         vm.deal(bob, 1000);
@@ -22,4 +20,23 @@ contract SpaceCoinTest is Test {
         coin = new SpaceCoin(treasury, coinCreator);
         vm.stopPrank();
     }
+
+    function test_transfer_SuccessfulTransferWithTax() public {
+    vm.prank(coinCreator);
+    coin.transfer(bob, 100);
+
+    assertEq(coin.balanceOf(bob), 98);
+    assertEq(coin.balanceOf(treasury), 350002);
+    assertEq(coin.balanceOf(coinCreator), 149900);
+}
+
+    function test_toggleTax_SuccessfulToggleTax() public {
+    vm.prank(coinCreator);
+    coin.toggleTax();
+
+    bool taxEnabled = coin.taxEnabled();
+    vm.stopPrank();
+
+    assert(!taxEnabled);
+}
 }
