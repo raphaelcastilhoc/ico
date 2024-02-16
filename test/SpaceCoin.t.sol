ce81f9e9f8dbccf3a9d236e5f2e28faa4656943e
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-02-16 16:34:26 -0300

    Added new unit test files

--- test/SpaceCoin.t.sol
+++ test/SpaceCoin.t.sol
@@ -22,31 +22,12 @@ contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
         vm.stopPrank();
     }
 
-    function test_transfer_SuccessfulTransferWithTax() public {
-    vm.prank(coinCreator);
-    coin.transfer(alice, 10);
-    
-    assertEq(coin.balanceOf(alice), 8);
-    assertEq(coin.balanceOf(treasury), 350002);
-    assertEq(coin.balanceOf(coinCreator), 149990);
-}
-
-    function test_fakeTransfer_SuccessfulFakeTransfer() public {
+    function test_fakeTransfer_SuccessfulTransfer() public {
     vm.prank(coinCreator);
     coin.fakeTransfer(alice, 10);
     
     assertEq(coin.balanceOf(alice), 8);
     assertEq(coin.balanceOf(treasury), 350002);
     assertEq(coin.balanceOf(coinCreator), 149990);
-}
-
-    /**
-* The problem with my previous attempt was that I was calling vm.stopPrank() without having a prank in progress. I should have removed that line of code because vm.prank() does not start a prank, it just sets the msg.sender for the next transaction.
-*/
-function test_toggleTax_SuccessfulToggleTax() public {
-    vm.prank(coinCreator);
-    coin.toggleTax();
-    bool taxStatus = coin.taxEnabled();
-    assert(!taxStatus);
 }
 }
