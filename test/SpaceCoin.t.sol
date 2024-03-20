bdacecadc9fce7c5109cedab6e26033296e8111f
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-03-20 15:11:41 -0300

    Added new test files

--- test/SpaceCoin.t.sol
+++ test/SpaceCoin.t.sol
@@ -1,10 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.19;
+
 import "forge-std/Test.sol";
 import "contracts/Ico.sol";
 import "./OlympixUnitTest.sol";
 
-contract SpaceCoinTest is OlympixUnitTest("SpaceCoin")  {
+contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
     address alice = address(0x456);
     address bob = address(0x789);
     address treasury = address(0xabc);
@@ -19,5 +20,24 @@ contract SpaceCoinTest is OlympixUnitTest("SpaceCoin")  {
         vm.startPrank(coinCreator);
         coin = new SpaceCoin(treasury, coinCreator);
         vm.stopPrank();
+    }
+
+    function test_transfer_SuccessfulTransferWithTax() public {
+        vm.prank(coinCreator);
+        coin.transfer(alice, 100);
+        
+        assertEq(coin.balanceOf(alice), 98);
+        assertEq(coin.balanceOf(treasury), 350002);
+        assertEq(coin.balanceOf(coinCreator), 149900);
+    }
+
+    /**
+    * The problem with my previous attempt was that I was calling vm.stopPrank() without having a prank in progress. I should have removed that line of code because vm.prank() does not start a prank, it just sets the msg.sender for the next transaction.
+    */
+    function test_toggleTax_SuccessfulToggleTax() public {
+        vm.prank(coinCreator);
+        coin.toggleTax();
+        bool taxStatus = coin.taxEnabled();
+        assert(!taxStatus);
     }
 }
