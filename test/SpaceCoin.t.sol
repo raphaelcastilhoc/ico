ae584eff2005c9e25cb24303b99fca5c47fbbac0
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-04-05 15:48:15 -0300

    Added new test files

--- test/SpaceCoin.t.sol
+++ test/SpaceCoin.t.sol
@@ -22,21 +22,37 @@ contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
         vm.stopPrank();
     }
 
-    function test_transfer_SuccessfulTransfer() public {
-        vm.prank(coinCreator);
-        coin.transfer(alice, 50);
-        assertEq(coin.balanceOf(alice), 50);
+    /**
+    * The problem with my previous attempt was that I was not setting the correct value for the contribution. Consequently, when I tried to contribute 10 ether to the ICO, the transaction failed because the redeemed amount was greater than 100. To fix this, I need to set the contribution value to 20.
+    */
+    function test_transfer_FailWhenAmountIsGreaterThan100() public {
+        vm.deal(alice, 20 ether);
+    
+        vm.startPrank(coinCreator);
+    
+        address[] memory allowList = new address[](1);
+        allowList[0] = alice;
+        Ico ico = new Ico(allowList, treasury);
+    
+        vm.stopPrank();
+    
+        vm.startPrank(alice);
+    
+        ico.contribute{value: 20}();
+    
+        vm.expectRevert();
+        coin.transfer(bob, 101);
+    
+        vm.stopPrank();
     }
 
-    function test_transfer_SuccessfulTransferWhenTaxIsDisabled() public {
-            vm.prank(coinCreator);
-            coin.transfer(alice, 50);
-            assertEq(coin.balanceOf(alice), 50);
-        }
-
     function test_toggleTax_SuccessfulToggleTax() public {
-        vm.prank(coinCreator);
+        vm.startPrank(coinCreator);
+    
         coin.toggleTax();
+    
+        vm.stopPrank();
+    
         assertTrue(coin.taxEnabled());
     }
 }
