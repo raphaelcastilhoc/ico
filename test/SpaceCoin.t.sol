25dd27b891eb7db87472cdee49130a8c80debe90
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-04-09 13:59:54 -0300

    Added new test files

--- test/SpaceCoin.t.sol
+++ test/SpaceCoin.t.sol
@@ -22,45 +22,62 @@ contract SpaceCoinTest is OlympixUnitTest("SpaceCoin") {
         vm.stopPrank();
     }
 
-    /**
-    * The problem with my previous attempt was that I didn't set the ico address in the SpaceCoin contract. Because of this, when the transfer function was called, it failed at the require(msg.sender == address(ico), "Only ICO can call this function"); line in the transfer function of the SpaceCoin contract. To fix this, I need to set the ico address in the SpaceCoin contract to the test contract's address. I can do this by calling vm.prank(address(this)) before creating the new SpaceCoin.
-    */
-    function test_transfer_SuccessfulTransfer() public {
+    // function test_transfer_SuccessfulTransferWhenTaxIsDisabled() public {
+    //     vm.startPrank(coinCreator);
+    
+    //     coin.toggleTax();
+    
+    //     uint initialSenderBalance = coin.balanceOf(coinCreator);
+    //     uint initialRecipientBalance = coin.balanceOf(treasury);
+        
+    //     uint amount = 99;
+    //     coin.transfer(treasury, amount);
+    
+    //     assertEq(coin.balanceOf(coinCreator), initialSenderBalance - amount);
+    //     assertEq(coin.balanceOf(treasury), initialRecipientBalance + amount);
+    
+    //     vm.stopPrank();
+    // }
+
+    function test_transfer_SuccessfulTransferWhenTaxIsEnabled() public {
         vm.startPrank(coinCreator);
-        coin = new SpaceCoin(treasury, coinCreator);
+    
+        uint initialSenderBalance = coin.balanceOf(coinCreator);
+        uint initialRecipientBalance = coin.balanceOf(treasury);
+        
+        uint amount = 99;
+        coin.transfer(treasury, amount);
+    
+        assertEq(coin.balanceOf(coinCreator), initialSenderBalance - amount);
+        assertEq(coin.balanceOf(treasury), initialRecipientBalance + amount);
+    
         vm.stopPrank();
-    
-        vm.prank(coinCreator);
-        coin.transfer(alice, 1);
-    //    assertEq(coin.balanceOf(alice), 1);
-    //    assertEq(coin.balanceOf(coinCreator), 149998);
-    //    assertEq(coin.balanceOf(treasury), 350001);
     }
-    
 
     function test_transfer_FailWhenAmountIsGreaterThan100() public {
         vm.startPrank(coinCreator);
     
         uint amount = 101;
         vm.expectRevert();
-        coin.transfer(alice, amount);
-    
-        vm.stopPrank();
-    }
-
-    function test_transfer_SuccessfulTransferWhenAmountIsLessThan100() public {
-        vm.startPrank(coinCreator);
-    
-        uint amount = 99;
-        coin.transfer(alice, amount);
+        coin.transfer(treasury, amount);
     
         vm.stopPrank();
     }
 
     function test_transfer_SuccessfulTransferWhenTaxIsDisabled() public {
-            vm.startPrank(coinCreator);
-            coin.toggleTax();
-            coin.transfer(alice, 1);
-            vm.stopPrank();
-        }
+        vm.startPrank(coinCreator);
+    
+        coin.toggleTax();
+    
+        uint initialSenderBalance = coin.balanceOf(coinCreator);
+        uint initialRecipientBalance = coin.balanceOf(treasury);
+        
+        uint amount = 99;
+        coin.transfer(treasury, amount);
+    
+        assertEq(coin.balanceOf(coinCreator), initialSenderBalance - amount);
+        assertEq(coin.balanceOf(treasury), initialRecipientBalance + amount);
+    
+        vm.stopPrank();
+    }
 }
