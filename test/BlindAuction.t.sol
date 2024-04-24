9ad9473d373053f15fd8adb10ee3b9c5d6a4b647
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-04-24 18:36:04 -0300

    Added new test files

---
+++ test/BlindAuction.t.sol
@@ -0,0 +1,143 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "contracts/BlindAuction.sol";
+import "./OlympixUnitTest.sol";
+
+contract BlindAuctionTest is OlympixUnitTest("BlindAuction") {
+    address alice = address(0x456);
+    address bob = address(0x789);
+    address payable beneficiary = payable(address(0xff));
+    BlindAuction blindAuction;
+
+    function setUp() public {
+        blindAuction = new BlindAuction(7 days, 1 days, beneficiary);
+
+        vm.deal(alice, 1000);
+        vm.deal(bob, 1000);
+    }
+
+    function test_bid_FailWhenItIsTooLateToBid() public {
+        vm.startPrank(alice);
+    
+        vm.warp(block.timestamp + 7 days);
+        vm.expectRevert(abi.encodeWithSelector(BlindAuction.TooLate.selector, blindAuction.biddingEnd()));
+        blindAuction.bid(keccak256(abi.encodePacked(uint(100), false, "")));
+    
+        vm.stopPrank();
+    }
+
+    function test_reveal_FailWhenItIsTooLate() public {
+        vm.startPrank(alice);
+    
+        vm.warp(block.timestamp + 8 days);
+        vm.expectRevert(abi.encodeWithSelector(BlindAuction.TooLate.selector, blindAuction.revealEnd()));
+        blindAuction.reveal(new uint256[](0), new bool[](0), new bytes32[](0));
+    
+        vm.stopPrank();
+    }
+
+    function test_reveal_FailWhenItIsTooEarly() public {
+        vm.startPrank(alice);
+    
+        vm.expectRevert(abi.encodeWithSelector(BlindAuction.TooEarly.selector, blindAuction.biddingEnd()));
+        blindAuction.reveal(new uint256[](0), new bool[](0), new bytes32[](0));
+    
+        vm.stopPrank();
+    }
+
+    function test_reveal_FailWhenValuesLengthIsDifferentFromBidsLength() public {
+        vm.startPrank(alice);
+    
+        blindAuction.bid{value: 100}(keccak256(abi.encodePacked(uint(100), false, "")));
+    
+        vm.warp(block.timestamp + 7 days + 1);
+    
+        vm.expectRevert();
+        blindAuction.reveal(new uint256[](0), new bool[](0), new bytes32[](0));
+    
+        vm.stopPrank();
+    }
+
+    function test_reveal_FailWhenFakesLengthIsDifferentFromBidsLength() public {
+        vm.startPrank(alice);
+    
+        blindAuction.bid{value: 100}(keccak256(abi.encodePacked(uint(100), false, "")));
+    
+        vm.warp(block.timestamp + 7 days + 1);
+    
+        vm.expectRevert();
+        blindAuction.reveal(new uint256[](1), new bool[](0), new bytes32[](1));
+    
+        vm.stopPrank();
+    }
+
+    function test_reveal_FailWhenSecretsLengthIsDifferentFromBidsLength() public {
+        vm.startPrank(alice);
+    
+        blindAuction.bid{value: 100}(keccak256(abi.encodePacked(uint(100), false, "")));
+    
+        vm.warp(block.timestamp + 7 days + 1);
+    
+        vm.expectRevert();
+        blindAuction.reveal(new uint256[](1), new bool[](1), new bytes32[](0));
+    
+        vm.stopPrank();
+    }
+
+    function test_reveal_FailWhenBlindedBidIsInvalid() public {
+        vm.startPrank(alice);
+    
+        blindAuction.bid{value: 100}(keccak256(abi.encodePacked(uint(100), false, "")));
+    
+        vm.warp(block.timestamp + 7 days + 1);
+    
+        blindAuction.reveal(new uint256[](1), new bool[](1), new bytes32[](1));
+    
+        assertEq(alice.balance, 900);
+    
+        vm.stopPrank();
+    }
+
+    function test_withdraw_FailWhenThereIsNoAmountToWithdraw() public {
+        vm.startPrank(alice);
+    
+        blindAuction.withdraw();
+    
+        vm.stopPrank();
+    }
+
+    function test_auctionEnd_FailWhenItIsTooEarly() public {
+        vm.startPrank(alice);
+    
+        vm.expectRevert(abi.encodeWithSelector(BlindAuction.TooEarly.selector, blindAuction.revealEnd()));
+        blindAuction.auctionEnd();
+    
+        vm.stopPrank();
+    }
+
+    function test_auctionEnd_SuccessfulEnd() public {
+        vm.startPrank(bob);
+    
+        vm.warp(block.timestamp + 8 days + 1);
+        blindAuction.auctionEnd();
+    
+        assertEq(beneficiary.balance, 0);
+        assertEq(blindAuction.ended(), true);
+    
+        vm.stopPrank();
+    }
+
+    function test_auctionEnd_FailWhenAuctionEndIsAlreadyCalled() public {
+        vm.startPrank(bob);
+    
+        vm.warp(block.timestamp + 8 days + 1);
+        blindAuction.auctionEnd();
+    
+        vm.expectRevert(BlindAuction.AuctionEndAlreadyCalled.selector);
+        blindAuction.auctionEnd();
+    
+        vm.stopPrank();
+    }
+}
---
+++ test/Purchase.t.sol
@@ -0,0 +1,72 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "contracts/Purchase.sol";
+import "./OlympixUnitTest.sol";
+
+contract PurchaseTest is OlympixUnitTest("Purchase") {
+    address alice = address(0x456);
+    address bob = address(0x789);
+    Purchase purchase;
+
+    function setUp() public {
+        purchase = new Purchase{value: 500}();
+
+        vm.deal(alice, 1000);
+        vm.deal(bob, 1000);
+    }
+
+    function test_abort_FailWhenStateIsNotCreated() public {
+        vm.startPrank(alice);
+    
+        purchase.confirmPurchase{value: 1000}();
+    
+        vm.expectRevert(Purchase.InvalidState.selector);
+        purchase.abort();
+    
+        vm.stopPrank();
+    }
+
+    function test_abort_FailWhenSenderIsNotSeller() public {
+        vm.startPrank(bob);
+    
+        vm.expectRevert(Purchase.OnlySeller.selector);
+        purchase.abort();
+    
+        vm.stopPrank();
+    }
+
+    function test_confirmReceived_FailWhenStateIsNotLocked() public {
+        vm.startPrank(bob);
+    
+        vm.expectRevert(Purchase.InvalidState.selector);
+        purchase.confirmReceived();
+    
+        vm.stopPrank();
+    }
+
+    function test_confirmReceived_FailWhenSenderIsNotBuyer() public {
+        vm.startPrank(alice);
+    
+        purchase.confirmPurchase{value: 1000}();
+    
+        vm.stopPrank();
+    
+        vm.startPrank(bob);
+    
+        vm.expectRevert(Purchase.OnlyBuyer.selector);
+        purchase.confirmReceived();
+    
+        vm.stopPrank();
+    }
+
+    function test_refundSeller_FailRefundWhenStateIsNotRelease() public {
+        vm.startPrank(alice);
+    
+        vm.expectRevert(Purchase.InvalidState.selector);
+        purchase.refundSeller();
+    
+        vm.stopPrank();
+    }
+}
