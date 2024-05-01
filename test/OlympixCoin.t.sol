2965098d1961c7b6d81b939aaaf16d5811ae866d
raphaelcastilhoc <47308861+raphaelcastilhoc@users.noreply.github.com>
2024-05-01 10:49:07 -0300

    Added new test files

--- test/OlympixCoin.t.sol
+++ test/OlympixCoin.t.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
-import "contracts/OlympixCoin.sol";
+import "src/OlympixCoin.sol";
 import "./OlympixUnitTest.sol";
 
 contract OlympixCoinTest is OlympixUnitTest("OlympixCoin") {
